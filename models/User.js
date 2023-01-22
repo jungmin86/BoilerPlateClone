f19b31a43af2c8e9cadde773f52cbdd46449311d
Jungmin Kim <jmkimm00@icloud.com>
2023-01-22 16:16:02 +0900

    hashed the password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = Schema({
     name: {
@@ -29,6 +31,23 @@ const userSchema = Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next) { //next 하면 index.js의 user.save 구문으로 들어감
+
+    var user = this;
+    //비밀번호를 암호화시킨다.
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash) { //hash -> 암호화된 비밀번호
+                if(err) return next(err);
+                user.password = hash; //교체
+                next(); //넥스트로 돌아가기
+            })
+        })
     }
 })
 
