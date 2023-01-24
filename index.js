7ea1c129567edd3caf1b429a328ba9fee7db5797
Jungmin Kim <jmkimm00@icloud.com>
2023-01-25 00:38:56 +0900

    authentication & logout routing

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-const { User } = require('./models/User');
+const { User } = require('./models/User.js');
 const bodyParser = require('body-parser'); //req.body로 정보를 받아옴
 const cookieParser = require('cookie-parser');
 
 const config = require('./config/key.js');
+const { auth } = require('./middleware/auth.js');
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,7 +31,7 @@ app.get('/', (req, res) => {
   res.send('This is Nodemon!');
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   //회원가입할 때 필요한 정보들을 client에서 가져오고, 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body); //req.body -> { id: Hello, password: 123}
   user.save((err, userInfo) => {
@@ -40,7 +41,7 @@ app.post('/register', (req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   //요청된 이메일을 데이터베이스에 있는지 찾는다.
   User.findOne({email: req.body.email}, (err, user) => {
     if(!user) {
@@ -51,19 +52,42 @@ app.post('/login', (req, res) => {
     }
     //요청된 이메일이 데이터베이스에 있으면, 맞는지 비밀번호인지 확인.
     user.comparePassword(req.body.password, (err, isMatch) => {
-      if(!isMatch) return res.json( { loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+      if(!isMatch) return res.json( { loginSuccess: false, message: "비밀번호가 틀렸습니다." });
     //비밀번호가 맞다면 해당 유저를 위한 Token 생성 
       user.generateToken((err, user) => {
         if(err) return res.status(400).send(err);
         //토큰을 저장한다. 어디에? 쿠키? 로컬스토리지? -> 쿠키
         res.cookie("x_auth", user.token)
-        .status(200).json({ loginSuccess: true, userId: user._id }) 
+        .status(200).json({ loginSuccess: true, userId: user._id, token:user.token }) 
       })
     })
   })
-  
 })
 
+app.get('/api/users/auth', auth, (req, res) => {
+  //여기까지 미들웨어를 통과해 왔다 -> Authentication이 true
+  res.status(200).json( {
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" },
+    (err, user) => {
+      if(err) return res.json({ success: false, err });
+      res.status(200).send( {
+        logoutSuccess: true
+      })
+    })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- models/User.js
+++ models/User.js
@@ -73,6 +73,19 @@ userSchema.methods.generateToken = function(cb) {
         cb(null, user);
     })
 }
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    //토큰  복호화
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에 클라이언트에서 가져온 토큰과 DB의 토큰이 일치하는지?
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
  
 const User = mongoose.model('User', userSchema);
 
