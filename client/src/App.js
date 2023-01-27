24e36f442756591c52c5628343bf11b3b4e0a12c
Jungmin Kim <jmkimm00@icloud.com>
2023-01-27 16:34:35 +0900

    react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
+   </Router>
   );
 }
 
 export default App;
+
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>LandingPage ddd</div>
+  )
+}
+
+export default LandingPage
