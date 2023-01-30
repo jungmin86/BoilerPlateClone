2eebfccc324b7a2da95dc4f8b1747244f061ab45
Jungmin Kim <jmkimm00@icloud.com>
2023-01-30 13:53:57 +0900

    login page 1

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -12,7 +12,12 @@ function LandingPage() {
   }, [])
   
   return (
-    <div>LandingPage geng</div>
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh'
+    }}>
+      <h2>시작 페이지</h2>
+      </div>
   )
 }
 
