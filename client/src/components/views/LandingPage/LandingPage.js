09369ca2dd1f3625c6e37d1def7aff3a3f266f51
Jungmin Kim <jmkimm00@icloud.com>
2023-01-31 18:57:06 +0900

    logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,9 @@
 import React, {useEffect} from 'react';
 import Axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+  const navigate = useNavigate();
 
   useEffect(() => {
     Axios.get('/api/hello')
@@ -10,13 +12,28 @@ function LandingPage() {
         console.log(response.data);
       })
   }, [])
-  
+
+  const onClickHandler = () => {
+    Axios.get('/api/users/logout')
+      .then(response => {
+        // console.log(response.data)
+        if(response.data.logoutSuccess) {
+          navigate('/login');
+        } else {
+          alert('로그아웃 실패 ');
+        }
+      })
+  }  
   return (
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
       </div>
   )
 }
